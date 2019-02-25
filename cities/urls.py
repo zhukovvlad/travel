2a7f00bce18862cd3f3afa6809c4603d8b0a6386
Vladimir Zhukov <zhukovvlad@yandex.ru>
2019-02-26 00:37:43 +0300

    Added some views

--- cities/urls.py
+++ cities/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import home
+from .views import home, CityDetailView
 
 urlpatterns = [
+    path('detail/<int:pk>/', CityDetailView.as_view(), name='detail'),
     path('', home, name='home'),
 ]
